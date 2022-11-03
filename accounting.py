62ab0ff8dd0255d56fdaa6b6ca808af8c280da60
PMKnapp001 <pmknapp001@outlook.com>
2022-11-03 19:09:28 -0400

    removed customers included in accounting.py and appended them to customer-orders.txt

--- accounting.py
+++ accounting.py
@@ -1,29 +1,3 @@
-melon_cost = 1.00
-
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
-
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
-
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
-
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
-
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
-
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
-
 def check_customer_payments(customer_file):
     payment_report = open(customer_file)
     MELON_COST = 1.00
